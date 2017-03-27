1e870df4bf3e57b3216e8251c19d55a9e2f6d16b
stevehe-campray <602258720@qq.com>
2017-03-27 10:59:13 +0800

    提交

---
+++ QQMusic/QQMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQMusic
+//
+//  Created by hejingjin on 17/3/27.
+//  Copyright © 2017年 teamsun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
